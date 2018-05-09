071a1a9989a9ed4057b6be590e95e5cc24de9ac4
Andrei Todorov <andrei@Andreis-MacBook-Pro.local>
2018-05-09 16:11:32 +0200

    Create HomeSlider component

---
+++ src/components/HomeSlider.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React from 'react'
+import Swiper from 'react-id-swiper'
+
+class HomeSlider extends React.Component {
+  render () {
+    return (
+      <Swiper>
+        <div>Slide 1</div>
+        <div>Slide 2</div>
+        <div>Slide 3</div>
+        <div>Slide 4</div>
+        <div>Slide 5</div>
+      </Swiper>
+    )
+  }
+}
+
+export default HomeSlider
