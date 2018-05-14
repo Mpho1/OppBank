441c7bdebd1b7728d64a185a9aa971964ecf7383
Sphiwe-Mybucks <sphiwe@mybucks.com>
2018-05-14 16:04:43 +0200

    cleaned up css

--- src/components/ContentsSection.js
+++ src/components/ContentsSection.js
@@ -6,12 +6,13 @@ class ContentsSection extends React.Component {
     constructor(props) {
         super(props)
     }
-
+    
+    
     render() {
         return (
-            <section className={`${ style.wrapper + ( this.props.bgImage ? " "+style.colorWhite : '' ) }`} style={ {backgroundImage:`url(${ this.props.bgImage })`}}>
-                <h2 className={style.h2}>{this.props.title}</h2>
-                <h4 className={style.h4}>{this.props.subtitle}</h4>
+            <section className={`${ style.wrapper + ( this.props.bgImage ? " "+style.sectionImageBackground : '' ) }`} style={ {backgroundImage:`url(${ this.props.bgImage })`}}>
+                <h2>{this.props.title}</h2>
+                <p>{this.props.subtitle}</p>
                 <div>
                     {this.props.children}
                 </div>
