9775d39cce1627aa8ab6a19e8bfe861f0ba57658
Sphiwe-Mybucks <sphiwe@mybucks.com>
2018-05-14 16:15:35 +0200

    ES lint conf

--- src/components/ContentsSection.js
+++ src/components/ContentsSection.js
@@ -3,22 +3,20 @@ import React from 'react'
 import style from './ContentsSection.module.scss'
 
 class ContentsSection extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    
-    render() {
-        return (
-            <section className={`${ style.wrapper + ( this.props.bgImage ? " "+style.sectionImageBackground : '' ) }`} style={ {backgroundImage:`url(${ this.props.bgImage })`}}>
-                <h2>{this.props.title}</h2>
-                <p>{this.props.subtitle}</p>
-                <div>
-                    {this.props.children}
-                </div>
-            </section>
-        )
-    }
+  render () {
+    return (
+      <section
+        className={`${style.wrapper + (this.props.bgImage ? ' ' + style.sectionImageBackground : '')}`}
+        style={ {backgroundImage: `url(${this.props.bgImage})`}}
+      >
+        <h2>{this.props.title}</h2>
+        <p>{this.props.subtitle}</p>
+        <div>
+          {this.props.children}
+        </div>
+      </section>
+    )
+  }
 }
 
 export default ContentsSection
