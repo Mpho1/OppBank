be8dddeea9f49fd03afa6a2bb711ade6473ddc15
Marvin <marvin@Marvins-MacBook-Pro.local>
2018-05-23 15:30:16 +0200

    imported the style for the component

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import style from './TextField.module.scss'
+
 const TextField = ({ placeholder }) => (
   <div>
     <form>
-      <input type="text" placeholder={placeholder}/>
+      <input type="text" placeholder={placeholder} className={style.inputField}/>
     </form>
   </div>
 )
