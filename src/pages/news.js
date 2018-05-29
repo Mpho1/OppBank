e8575e357537554c1bc8138ee3833f6dfe080667
Marvin <marvin@Marvins-MacBook-Pro.local>
2018-05-29 12:48:13 +0200

    news section in progress

--- src/pages/news.js
+++ src/pages/news.js
@@ -2,26 +2,31 @@ import React from 'react'
 import ThirdsColumns from '../components/ThirdsColumns'
 import CategoryBlock from '../components/CategoryBlock'
 import FinancialResultsBlock from '../components/FinancialResultsBlock'
+import SideNews from '../components/SideNews'
+import BlogMainContent from '../components/BlogMainContent'
 
 const News = () => (
   <div>
     <br/><br/><br/><br/><br/><br/>
     <ThirdsColumns>
       <CategoryBlock>
-        Test 1
+        <SideNews/>
       </CategoryBlock>
 
-      <div>
-        Contents
-      </div>
+      <BlogMainContent/>
 
-      <FinancialResultsBlock data={[
-        {title: 'first title', href: 'http://first.link.com'},
-        {title: 'second title', href: 'http://second.link.com'},
-        {title: 'third title', href: 'http://third.link.com'},
-        {title: 'forth title', href: 'http://forth.link.com'}
-      ]}/>
+      <div>
+        <FinancialResultsBlock
+          data={[
+            {title: 'Opportunity Bank summary of the financial statements - December 2017', href: 'http://first.link.com'},
+            {title: 'Opportunity Bank summary of the financial statements - December 2017', href: 'http://second.link.com'},
+            {title: 'Opportunity Bank summary of the financial statements - December 2017', href: 'http://third.link.com'},
+            {title: 'Opportunity Bank summary of the financial statements - December 2017', href: 'http://forth.link.com'}
+          ]}
+        />
+      </div>
     </ThirdsColumns>
+    fjfjfjf
   </div>
 )
 
