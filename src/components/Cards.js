6b9d6fd795735d7a6ddd8090eb8a1ecd33cc934d
Mpho Mamahlodi <mphomamahlodi@Mphos-MacBook-Air.local>
2018-06-20 10:48:09 +0200

    fixed conflicts

---
+++ src/components/Cards.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './card.module.scss'
+
+const CardContent = ({image, title}) => (
+  <div>
+    <div className={styles.CardImage}>
+      <img src={image} />
+    </div>
+    <h4>
+      {title}
+    </h4>
+  </div>
+)
+
+const Cards = () => (
+  <div className={styles.CardContainer}>
+    <div className={styles.CardInfo}>
+      <CardContent
+        image={require('../img/contact_us.svg')}
+        title='Contact us'
+      />
+    </div>
+    <div className={styles.CardInfo}>
+      <CardContent
+        image={require('../img/find_us.svg')}
+        title='Find us'
+      />
+    </div>
+    <div className={styles.CardInfo}>
+      <CardContent
+        image={require('../img/feedback.svg')}
+        title='Feedback'
+      />
+    </div>
+  </div>
+)
+
+export default Cards
---
+++ src/pages/find-us.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PageHeader from '../components/PageHeader'
+import ThirdsColumns from '../components/ThirdsColumns'
+
+import Cards from '../components/Cards'
+
+const SecondPage = () => (
+  <div>
+    <PageHeader
+      title="Find Us"
+      subtitle="Got more questions or would you rather feel more comfortable just visiting one of our branches?"/>
+    <ThirdsColumns breakOrder="213">
+      <Cards/>
+    </ThirdsColumns>
+  </div>
+)
+
+export default SecondPage
