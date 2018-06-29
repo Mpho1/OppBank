5456d964b3488e3144eaae32b9e2aa5482b30bd7
Marvin <marvin@Marvins-MacBook-Pro.local>
2018-06-29 17:29:31 +0200

    savings account display in item swiper

--- src/pages/savings.js
+++ src/pages/savings.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import SavingsAccount from '../components/SavingsAccount'
-import style from './savings.module.scss'
 
 import PageHeader from '../components/PageHeader'
 import ItemSwiper from '../components/ItemSwiper'
+import ContentsSection from '../components/ContentsSection'
 
 const Savings = () => (
   <div>
@@ -11,44 +11,58 @@ const Savings = () => (
       title="SAVINGS ACCOUNT"
       subtitle="Let that brush dance around there and play. God gave you this gift of imagination. Use it, Every single thing in the world has its own personality">
     </PageHeader>
-    <ItemSwiper>
-      <div className={style.savingsItem}>
-        <SavingsAccount
-          image={require('../img/target.svg')}
-          header="Fixed Term"
-          type="Deposit Account"
-          text="Gain control over your finances with a free cash withdrawal per month"/>
-      </div>
-      <div className={style.savingsItem}>
-        <SavingsAccount
-          image={require('../img/target.svg')}
-          header="Fixed Term"
-          type="Deposit Account"
-          text="Gain control over your finances with a free cash withdrawal per month"/>
-      </div>
-      <div className={style.savingsItem}>
-        <SavingsAccount
-          image={require('../img/target.svg')}
-          header="Fixed Term"
-          type="Deposit Account"
-          text="Gain control over your finances with a free cash withdrawal per month"/>
-      </div>
-      <div className={style.savingsItem}>
-        <SavingsAccount
-          image={require('../img/target.svg')}
-          header="Fixed Term"
-          type="Deposit Account"
-          text="Gain control over your finances with a free cash withdrawal per month"/>
-      </div>
-      <div className={style.savingsItem}>
-        <SavingsAccount
-          image={require('../img/target.svg')}
-          header="Fixed Term"
-          type="Deposit Account"
-          text="Gain control over your finances with a free cash withdrawal per month"/>
-      </div>
-    </ItemSwiper>
+    <ContentsSection>
+      <ItemSwiper>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms4"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms3"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms5"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms6"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms7"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+        <div style={style}>
+          <SavingsAccount
+            image={require('../img/target.svg')}
+            header="Fixed Terms8"
+            type="Deposit Account"
+            text="Gain control over your finances with a free cash withdrawal per month"/>
+        </div>
+      </ItemSwiper>
+    </ContentsSection>
   </div>
 )
+
+const style = {
+  width: '265px',
+  marginTop: '-3em'
+}
 
 export default Savings
