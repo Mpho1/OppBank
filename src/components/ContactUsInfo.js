7b17971af52e319ccdd7e6f6d6be181e3a81484a
Mpho <MphoM@mybucks.com>
2018-07-24 15:29:48 +0200

    responsive contact us page

--- src/components/ContactUsInfo.js
+++ src/components/ContactUsInfo.js
@@ -38,28 +38,30 @@ class ContactUsInfo extends React.Component {
           CONTACT US
         </h4>
         <h4>
-          <img src={require('../img/headoffice_green.svg')} className={style.icon} />
-        Head Office
+          <img src={require('../img/headoffice_green2.svg')} className={style.icon} />
+          Head Office
         </h4>
         <p className={style.description}>
           {Items}
         </p>
         <h4>
-          <img src={require('../img/contact_green.svg')} className={style.icon}/>
-        Contact
+          <img src={require('../img/Page1.svg')} className={style.icon}/>
+          Contact
         </h4>
         <p className={style.description}>0414 236 724 / 342 726</p>
         <h4>
-          <img src={require('../img/email_green.svg')} className={style.icon}/>
-        Email
+          <img src={require('../img/email_green2.svg')} className={style.icon}/>
+          Email
         </h4>
         <p className={style.description}>info@opportunitybank.co.ug </p>
         <p className={style.Findus}>Find us on social media </p>
-        <p>
-          <img src={require('../img/facebook.svg')} className={style.socialIcon} />
-          <img src={require('../img/twitter.svg')} className={style.socialIcon} />
-          <img src={require('../img/whatsapp.svg')} className={style.socialIcon} />
-        </p>
+        <div>
+          <p>
+            <img src={require('../img/facebook.svg')} className={style.socialIcon} />
+            <img src={require('../img/twitter.svg')} className={style.socialIcon} />
+            <img src={require('../img/whatsapp.svg')} className={style.socialIcon} />
+          </p>
+        </div>
         <div className={style.placeholders}>
           <p>
             <input type="text" name="NAME" placeholder= "NAME" className={style.inputBox} onChange={this.handleChange.bind(this)}/>
@@ -71,7 +73,7 @@ class ContactUsInfo extends React.Component {
             <input type="number" name="CONTACT NUMBER" placeholder="CONTACT NUMBER" className={style.inputBox} onChange={this.handleChange.bind(this)}/>
           </p>
           <p>
-            <input type="text" name="Your Query" placeholder="Your Query" className={style.inputBoxQuery} onChange={this.handleChange.bind(this)}/>
+            <textarea id="subject" name="Your Query" placeholder="Your Query" className={style.inputBoxQuery} onChange={this.handleChange.bind(this)}/>
           </p>
           <input type="submit" value="SUBMIT" className={style.submitButton}/>
         </div>
